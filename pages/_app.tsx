33ac9f0b47e69cb4e8ca979cddd3fc16e50be6d0
pashkouski <satiym@gmail.com>
2023-01-27 23:27:46 +0300

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>Create Next App</title>
+                <link key={1} rel="icon" href="/favicon.ico"/>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+                    rel="stylesheet"/>
+            </Head>
+            <Component {...pageProps} />
+        </>)
 }
